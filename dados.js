23d305f4df1c6871db333f0f7fda1f7c8cfb15d5
Gustavo <gucs_13@hotmail.com>
2024-09-16 16:50:54 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -4,20 +4,32 @@ const agentes = [
     nome: "Sage",
     descricao: "Sage é um dos agentes mais populares e versáteis de Valorant, conhecida por suas habilidades de cura e controle de área. Como uma Sentinela, ela é crucial para proteger a equipe e garantir a sobrevivência dos aliados.",
     link: "https://playvalorant.com/pt-br/agents/sage/",
-    imagem:"img/Sage.png"
+    imagem:"Sage.png"
 },
   
 {
     nome: "Jett",
     descricao: "Jett é uma Duelista ágil e mortal, capaz de realizar manobras acrobáticas e eliminar inimigos com precisão. Suas habilidades de mobilidade e dano a curto alcance a tornam uma força a ser considerada.",
     link: "https://playvalorant.com/pt-br/agents/jett/",
-    imagem:"img/Jett.png"
+    imagem:"Jett.png"
 },
   
 {
     nome: "Phoenix",
     descricao: "Phoenix é um Duelista arrojado e agressivo, que usa o fogo para infligir dano e criar oportunidades para sua equipe. Suas habilidades de teleporte e cura o tornam um incêndio constante no campo de batalha.",
     link: "https://playvalorant.com/pt-br/agents/phoenix/",
-    imagem:"img/Phoenix.png"
-}
+    imagem:"Phoenix.png"
+},
+{
+    nome: "Brimstone",
+    descricao: "Brimstone é um Controlador que domina o campo de batalha com suas habilidades de fogo.",
+    link: "https://playvalorant.com/pt-br/agents/brimstone/",
+    imagem:"Brimstone.png"
+  },
+  {
+    nome: "Viper",
+    descricao: "Viper, a química dos Estados Unidos, emprega uma variedade de dispositivos químicos venenosos para controlar o campo de batalha e prejudicar a visão do inimigo. Se as toxinas não matarem a presa, seus jogos mentais certamente o farão.",
+    link: "https://playvalorant.com/pt-br/agents/viper/",
+    imagem:"Viper.png"
+  },
 ];
